91fb5b44656cd6bdfb105d042191bcdecb55e924
João Henrique <joaohenrique.p.almeida@gmail.comm>
2022-11-21 12:40:41 -0300

    refactoring main.js file and add package.json file for project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # rpg-trade-system
+
+## Como rodar
+rodar o server de dados JSON Server
+```
+npx json-server --watch pokemons.json
+```
+
+É necessário ter esse server rodando para pegar as informações dos pokemons.
+
+Abrir o arquivo index.html que com o import do arquivo main.js
--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 
-import pokemon from "./pokemons.json" assert {type: 'json'};
+//Isso é uma função
+async function getPokemonList() {
 
-const pokemonName = {}
+    let pokemons = await fetch('http://localhost:3000/pokemon')
+        .then((response) =>
+            response.json()
+        )
+        .then(data => {
+            return data;
+        })
+        .catch((error) => console.log("Error", error));
 
+    return pokemons
+}
+
+// Função main executar como a principal do programa
+(async function main() {
+
+    // Usando a função
+    // AWAIT é usado para realizar uma espera, 
+    // essa função vai pegar informações e precisa 
+    // esperar o retorno da requisição
+    const pokemonsList = await getPokemonList();
+
+    console.log("List all pokemons", pokemonsList)
+    const pokeNames = pokemonsList.map((pokemons) => { return pokemons.name })
+
+    console.log("Pokemon names", pokeNames);
+
+})();
